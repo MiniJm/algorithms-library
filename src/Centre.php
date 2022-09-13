02dc6305e621ec0a37da1b297a6352d8f5dff2cf
wangweijie <844048869@qq.com>
2022-09-13 17:51:48 +0800

    build: library init

---
+++ src/Centre.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lz\AlgorithmsLibrary;
+
+/**
+ * 算法中心库
+ */
+class Centre
+{
+    /**
+     * 计算两个参数运算
+     * @param int $a
+     * @param int $b
+     * @return int
+     */
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
